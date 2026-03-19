7f7eccd63bdf18f6c2c6165793d6aeb0869d5fe3
NicolasCruz <jhonathannicolascruz@gmail.com>
2026-03-19 12:31:23 -0500

    Hoy Hice un programa el cual haga calculos de promedio de 20 estudiantes y 20 notas con calificaciones de 0 - 20

---
+++ Ejercicios en c++/Untitled-1.cpp
@@ -0,0 +1,78 @@
+/*
+Nombre:Jhonathan Cruz
+Fecha:18/3/2026
+
+
+
+//Enunciado:Una academia desea llevar un control basico de las notas de un grupo de estudiantes Mediante un programa en C++.
+Se requiere desarrollar un sistema que permita registrar y analizar las calificaciones de un grupo pequeño de estudiantes usando arreglos unidimensionales y funciones.
+
+E/P/S
+
+
+Entrada:
+ 1.Resivir la cantidad de estudiantes 
+ 2.Resivir nombre de cada estudiante
+ 3.Resivir nota de cada estudiante
+ 4.nombre a buscar
+
+Proceso:
+ 1.Validar que los valores no sean numeros negativos
+ 2.Calcular promedio general
+ 3.Calcular nota mayor y menor de cada estudiante
+ 4.Mostrar reporte de aprobados y reprobados
+ 5.Buscar las notas menores y mayores
+ 6.Permitir consultar un estudiante por nombre
+
+Salida:
+1.Lista de estudiantes y notas
+2.Promedio genral
+3.Nota mayor y nota menor
+4.cantodad de aprobados y reprobados
+5. Resultado de busqueda por nombre
+
+#include <iostream>
+using namespace std;
+int main(){
+
+//Declaramos variables para resivir cantidad de notas,estudiantes y las calificasiones
+double calificaciones [20] [10]
+//la calificacion es de 0 - 20;
+int CantidadDeNotas;
+//La cantidad de notas es de 20
+int CantidadDeEstudiantes;
+//la cantidad de estudiantes es de 20;
+}
+ 
+cout <<"====MENU===="
+cout <<"Ingresa la cantidad de estudiantes"(Maximo 20)
+cin >> CantidadDeEstudiantes;
+//La cantidad de estudiantes es de 20
+cout <<"ingresa la cantidad de notas"
+cin >> CantidadDeNotas
+//La cantidad de notas es de 20
+Cout<< "ingrese la calificacion de cada estudiante"
+//Recorrer estudiantes
+for (int i =0; i <CantidadDeEstudiantes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
