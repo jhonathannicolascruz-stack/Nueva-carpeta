e64faaa680ad75493f5d9119ff7d6109b664ef14
NicolasCruz <jhonathannicolascruz@gmail.com>
2026-03-22 16:58:38 -0500

    termine el EPS del proyecto y ahora estoy empezando a hacer el codigo

--- Ejercicios en c++/Proyecto 1.cpp
+++ Ejercicios en c++/Proyecto 1.cpp
@@ -5,11 +5,33 @@ Enunciado:Realizar un sistema el cual permita que el usuario ingrese un valor en
 (celsius) y el sistema lo transforme en Fahrenheit
 
 E/P/S:
-1.Pedir el un valor entero o decimal al usuario
+
+
+Entrada:
+
+ 1.Pedir el un valor entero o decimal al usuario
+ 2.Validar que el valor no sea un numero negativo 
+
+Proceso:
+ 
+ 1.Realizar su respectiva operacion con el valor ingresado (Celsius * 9/5) + 32
+ 
+Salida:
+ 
+ 1.Mostrar el resultado
+
+
+# include <iostream>;
+using namespace std;
+int main(){
+
+//Declaramos variables
 
 
 
 
+
+}
 
 
 
