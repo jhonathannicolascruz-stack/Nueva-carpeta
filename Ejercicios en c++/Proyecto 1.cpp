14dd63b09e3a551461b9a6b8ca5244d039a02da7
NicolasCruz <jhonathannicolascruz@gmail.com>
2026-03-22 16:51:12 -0500

    Hoy estoy empezando a hacer un progrma el cual permita al usuario ingresar un valor entero o decimal (celcius) y el programa los transforme a Fahrenheit

--- Ejercicios en c++/Proyecto 1.cpp
+++ Ejercicios en c++/Proyecto 1.cpp
@@ -1,7 +1,11 @@
 /*
 Autor:Jhonathan Nicolas 
 Fecha: 20/3/2026
-Enunciado:Realizar un sistema el cual permita que el usuario ingrese 
+Enunciado:Realizar un sistema el cual permita que el usuario ingrese un valor entero o decimal
+(celsius) y el sistema lo transforme en Fahrenheit
+
+E/P/S:
+1.Pedir el un valor entero o decimal al usuario
 
 
 
